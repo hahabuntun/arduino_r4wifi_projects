2c8008defe15b3704bdd072f47469dabeea6ae0d
vitaly <hahabuntun@gmail.com>
2026-02-24 22:50:47 +0300

    Blink 2 leds

---
+++ blink_2_leds/blink_2_leds.ino
@@ -0,0 +1,17 @@
+int led1 = 4;
+int led2 = 5;
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  delay(1000);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  delay(1000);
+}
